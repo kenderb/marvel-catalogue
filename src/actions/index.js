57703a15b24c1ff27aa2899c18b8cd4518bc7efd
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-20 15:07:33 -0500

    Increase the limit of characters per request

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,10 +4,9 @@ export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
 
 export const fetchCharacters = (character = '') => async dispatch => {
   try {
-    const response = await developerMarvel.get(`/characters?${character ? `name=${character}&` : ''}${QUERY}`, {
-      orderBy: 'name',
+    const response = await developerMarvel.get(`/characters?${character ? `name=${character}&` : ''}limit=30&${QUERY}`, {
+      orderBy: 'modified',
     });
-    console.log('submit');
     dispatch({ type: FETCH_CHARACTERS, payload: response.data.data.results });
     return true;
   } catch (error) {
