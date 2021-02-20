6b3c9fb98f00860db5e70c87552ac07e81cad00c
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-20 17:15:54 -0500

    Add the list of Creators

--- src/containers/ComicForm.js
+++ src/containers/ComicForm.js
@@ -3,6 +3,21 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchComics } from '../actions';
 
+const listOfCreators = [
+  'Federico Blee',
+  'Sebastian Carrillo',
+  'John Tyler Christopher',
+  'Christina Harrington',
+  'Vc Cory Petit',
+  'Mariko Tamaki',
+  'Tom Brevoort',
+  'C Cafu',
+  'Christopher Cantwell',
+  'Vc Joe Caramagna',
+  "Frank D'ARMATA",
+  'Alex Ross',
+];
+
 const ComicForm = ({ fetchComics }) => {
   const [inputValue, setInputValue] = useState('');
   const handleSumit = e => {
@@ -17,6 +32,11 @@ const ComicForm = ({ fetchComics }) => {
     <form onSubmit={e => handleSumit(e)}>
       <input type="text" value={inputValue} onChange={e => handleOnChnage(e)} />
       <button type="submit">Submit</button>
+      <select>
+        {listOfCreators.map(creator => (
+          <option value={creator} key={creator}>{creator}</option>
+        ))}
+      </select>
     </form>
   );
 };
