8303c9683afc1c1565b307527cc1f01a8095629f
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-20 13:15:57 -0500

    Fix the keys variables

--- src/apis/developerMarvel.js
+++ src/apis/developerMarvel.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { nanoid } from 'nanoid';
 import md5 from 'md5';
 
+const { REACT_APP_PUBLIC_KEY: PUBLIC_KEY, REACT_APP_PRIVATE_KEY: PRIVATE_KEY } = process.env;
 const TS = nanoid(10);
-const PRIVATE_KEY = '';
-const PUBLIC_KEY = '';
+
 const HASH = md5(TS + PRIVATE_KEY + PUBLIC_KEY);
 
 export const QUERY = `ts=${TS}&apikey=${PUBLIC_KEY}&hash=${HASH}`;
