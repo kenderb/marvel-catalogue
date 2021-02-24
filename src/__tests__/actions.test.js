d2cd346b51f4eaba6580d27850e29493e3fbd5b4
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-24 07:15:40 -0500

    Add negative tests tor the actions tests

--- src/__tests__/actions.test.js
+++ src/__tests__/actions.test.js
@@ -8,16 +8,32 @@ describe('Action Types for Comics', () => {
     const action = fetchComicsSuccess();
     expect(action.type).toStrictEqual('FETCH_COMICS');
   });
+  it('fetchComicsSuccess return the right action type', () => {
+    const action = fetchComicsSuccess();
+    expect(action.type).not.toBe('');
+  });
   it('fetchComicsLoading return the right action type', () => {
     const action = fetchComicsLoading();
     expect(action.type).toStrictEqual('LOADING_COMICS');
+  });
+  it('fetchComicsLoading return the right action type', () => {
+    const action = fetchComicsLoading();
+    expect(action.type).not.toBe('');
   });
   it('fetchComicsError return the right action payload', () => {
     const action = fetchComicsError();
     expect(action.type).toStrictEqual('ERROR_COMICS');
   });
+  it('fetchComicsError return the right action payload', () => {
+    const action = fetchComicsError();
+    expect(action.type).not.toBe('');
+  });
   it('filterByCreatorSuccess return the right action payload', () => {
     const action = filterByCreatorSuccess('comic one');
     expect(action.type).toStrictEqual('FILTER_COMICS');
+  });
+  it('filterByCreatorSuccess return the right action payload', () => {
+    const action = filterByCreatorSuccess('comic one');
+    expect(action.type).not.toBe('');
   });
 });
